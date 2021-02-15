a292ee64abd10d36350cc78f62ee02ee1f5fc4ea
Emlemaudit <lecomtemrik@gmail.com>
2021-02-15 20:46:37 +0100

    Initial

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+}
