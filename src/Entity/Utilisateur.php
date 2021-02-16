1cb2825be7d77492b8f3521c2a2c58868d2c0d1b
Emlemaudit <lecomtemrik@gmail.com>
2021-02-16 20:22:03 +0100

    Nucleos #2 fosuser to nucleos (installation/configuration)

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -4,106 +4,24 @@ namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Nucleos\UserBundle\Model\User as BaseUser;
 
 /**
  * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @ORM\Table(name="Utilisateur")
  */
-class Utilisateur
+class Utilisateur extends BaseUser
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $nom;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $prenom;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $motDePasse;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $dateNaissance;
-
-    public function getId(): ?int
+    public function __construct()
     {
-        return $this->id;
-    }
-
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getMotdepasse(): ?string
-    {
-        return $this->motDePasse;
-    }
-
-    public function setMotdepasse(string $motdepasse): self
-    {
-        $this->motDePasse = $motdepasse;
-
-        return $this;
-    }
-
-    public function getDateNaissance(): ?\DateTimeInterface
-    {
-        return $this->dateNaissance;
-    }
-
-    public function setDateNaissance(\DateTimeInterface $dateNaissance): self
-    {
-        $this->dateNaissance = $dateNaissance;
-
-        return $this;
+        parent::__construct();
+        // your own logic
     }
 }
