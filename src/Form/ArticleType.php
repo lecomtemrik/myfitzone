90648d190db6b45cf82e2a74c73d6d695127f509
Emlemaudit <lecomtemrik@gmail.com>
2021-02-16 01:24:27 +0100

    Doctrine #1 - Création du controller article

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('slug')
+            ->add('description')
+            ->add('contenu')
+            ->add('dateCreation')
+            ->add('dateModification')
+            ->add('utilisateur')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
