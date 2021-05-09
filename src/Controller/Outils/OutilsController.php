39ed96d59afb00efe1b95961e3f7bdd48922f757
Emlemaudit <lecomtemrik@gmail.com>
2021-05-09 17:19:59 +0200

    ajout des calculatrices et outils sections Calculatrices de la navigation #11

--- src/Controller/Outils/OutilsController.php
+++ src/Controller/Outils/OutilsController.php
@@ -72,4 +72,156 @@ class OutilsController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/poids-ideal", name="poidsIdeal", methods={"GET","POST"})
+     */
+    public function piCalcul(Request $request): Response
+    {
+
+        $mesure = new Mesure();
+        $form = $this->createForm(MesureType::class, $mesure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($mesure->getSexe()==true){
+                $poidsIdeal = round($mesure->getTaille()-100-(($mesure->getTaille()-150)/4));
+            }else {
+                $poidsIdeal = round($mesure->getTaille()-100-(($mesure->getTaille()-150)/2.5));
+            }
+
+            return $this->render('outils/calculatrice_poids_ideal.html.twig', [
+                'poidsIdeal' => $poidsIdeal,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        return $this->render('outils/calculatrice_poids_ideal.html.twig', [
+            'poidsIdeal' => null,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/calories-par-jour", name="caloriesJour", methods={"GET","POST"})
+     */
+    public function caloriesJourCalcul(Request $request): Response
+    {
+
+        $mesure = new Mesure();
+        $metaBase = null;
+        $besoinCalorique = null;
+        $form = $this->createForm(MesureType::class, $mesure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($mesure->getSexe()==true){
+                $metaBase = round(66.5+(13.75*$mesure->getPoids())+(5*$mesure->getTaille())-(6.77*$mesure->getAge()));
+            }else {
+                $metaBase = round(655+(9.56*$mesure->getPoids())+(1.85*$mesure->getTaille())-(4.67*$mesure->getAge()));
+            }
+
+            if ($mesure->getNiveauActivite() == 'sedentaire'){
+                $besoinCalorique = round($metaBase*1.37,1);
+            }elseif ($mesure->getNiveauActivite() == 'actif'){
+                $besoinCalorique = round($metaBase*1.55,1);
+            }elseif ($mesure->getNiveauActivite() == 'sportif'){
+                $besoinCalorique = round($metaBase*1.80,1);
+            }
+
+            return $this->render('outils/calculatrice_calories_jour.html.twig', [
+                'metabolismeBase' => $metaBase,
+                'besoinCalorique' => $besoinCalorique,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        return $this->render('outils/calculatrice_calories_jour.html.twig', [
+            'metabolismeBase' => $metaBase,
+            'besoinCalorique' => $besoinCalorique,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/calories-par-jour-par-repas", name="caloriesJourRepas", methods={"GET","POST"})
+     */
+    public function caloriesJourRepasCalcul(Request $request): Response
+    {
+
+        $mesure = new Mesure();
+        $metaBase = null;
+        $besoinCalorique = null;
+        $form = $this->createForm(MesureType::class, $mesure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($mesure->getSexe()==true){
+                $metaBase = round(66.5+(13.75*$mesure->getPoids())+(5*$mesure->getTaille())-(6.77*$mesure->getAge()));
+            }else {
+                $metaBase = round(655+(9.56*$mesure->getPoids())+(1.85*$mesure->getTaille())-(4.67*$mesure->getAge()));
+            }
+
+            if ($mesure->getNiveauActivite() == 'sedentaire'){
+                $besoinCalorique = round($metaBase*1.37,1);
+            }elseif ($mesure->getNiveauActivite() == 'actif'){
+                $besoinCalorique = round($metaBase*1.55,1);
+            }elseif ($mesure->getNiveauActivite() == 'sportif'){
+                $besoinCalorique = round($metaBase*1.80,1);
+            }
+
+            return $this->render('outils/calculatrice_calories_jour_repas.html.twig', [
+                'besoinCalorique' => $besoinCalorique,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        return $this->render('outils/calculatrice_calories_jour_repas.html.twig', [
+            'besoinCalorique' => $besoinCalorique,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/hydratation-quotidienne", name="hydratation", methods={"GET","POST"})
+     */
+    public function qtEau(Request $request): Response
+    {
+
+        $mesure = new Mesure();
+        $quantiteEau = null;
+        $form = $this->createForm(MesureType::class, $mesure);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($mesure->getAge() < 60){
+                $quantiteEau = round($mesure->getPoids()*30);
+            }else {
+                $quantiteEau = round($mesure->getPoids()*25);
+            }
+
+            return $this->render('outils/calculatrice_hydratation.html.twig', [
+                'quantiteEau' => $quantiteEau,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        return $this->render('outils/calculatrice_hydratation.html.twig', [
+            'quantiteEau' => $quantiteEau,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/body-visualizer", name="bodyVisualizer", methods={"GET","POST"})
+     */
+    public function bodyVisualizer(): Response
+    {
+
+        return $this->render('outils/body_visualizer.html.twig');
+    }
+
 }
